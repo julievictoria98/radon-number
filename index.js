d384580a27e4b678232fd8c71d643c31418cdd8e
julievictoria98 <jura0002@stud.kea.dk>
2024-09-16 14:19:57 +0200

    kommentar

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// console.log("Math.random",Math.floor( Math.random()*(3 + 1)));
-
-// 3 gør at det er mellem 0 0g 2
-// men hvis man plusser med 1 så kommer 3 med
-
-// console.log("Math.random",Math.ceil( Math.random()*3));
-
-// den her gør det samme som ovenover, bare en anden metode
-
-// let RandomNumber = Math.floor( Math.random()*(100 + 1));
-// console.log(RandomNumber);
 
 document.querySelector("#button").addEventListener("click", getNumber)
 
@@ -18,13 +7,5 @@ function getNumber(){
     document.querySelector("#output").innerHTML = RandomNumber;
 }
 
-// function guessNumber(){
-//     let guess = document.querySelector("#input").value;
-//     console.log(guess);
-//     if(guess == RandomNumber){
-//         document.querySelector("#output").innerHTML = "You guessed right";
-//     } else {
-//         document.querySelector("#output").innerHTML = "You guessed wrong";
-//     }
-// }
+
 
